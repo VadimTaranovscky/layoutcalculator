17e0341c37d381a29edece4178fc7c97873cd733
VadimTaranovscky <43213486+VadimTaranovscky@users.noreply.github.com>
2020-02-20 13:51:58 +0400

    Update script.js

--- CalculatorHTML/script/script.js
+++ CalculatorHTML/script/script.js
@@ -12,6 +12,8 @@ const DATA = {
     deadLinePercent: [20, 17, 15]
 };
 
+const arrTitles = ['день', 'дня', 'дней'];
+
 const startButton = document.querySelector('.start-button');
 const firstScreen = document.querySelector('.first-screen');
 const mainForm = document.querySelector('.main-form');
@@ -20,12 +22,24 @@ const endButton = document.querySelector('.end-button');
 const totalElem = document.querySelector('.total');
 const fastRange = document.querySelector('.fast-range');
 const totalPriceSum = document.querySelector('.total_price__sum');
-
+const typeSite = document.querySelector('.type-site');
+const maxDeadLine = document.querySelector('.max-deadline');
 const adapt = document.querySelector('#adapt');
 const mobileTemplates = document.querySelector('#mobileTemplates');
+const rangeDeadline = document.querySelector('.range-deadline');
+const deadlineValue = document.querySelector('.deadline-value');
+
+const inputArr = document.querySelectorAll('.switcher .calc-handler');
+const checkboxLabel = document.querySelectorAll('.switcher .checkbox-label');
+// console.log(inputArr);
+// console.log(checkboxLabel);
 mobileTemplates.disabled = true;
 
-console.dir(totalElem);
+function declOfNum(n, titles) {
+    return n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+        0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+}
+
 function showElem(elem) {
     elem.style.display = 'block';
 }
@@ -34,10 +48,22 @@ function hideElem(elem) {
     elem.style.display = 'none';
 }
 
+function renderTextContent(total, site, maxDay, minDay) {
+    totalPriceSum.textContent = total;
+    typeSite.textContent = site;
+    maxDeadLine.textContent = declOfNum(maxDay, arrTitles);
+    rangeDeadline.min = minDay;
+    rangeDeadline.max = maxDay;
+    deadlineValue.textContent = declOfNum(rangeDeadline.value, arrTitles);
+}
+
 function priceCalculation(element) {
     let result = 0;
     let index = 0;
     let options = [];
+    let site = '';
+    let maxDeadLineDay = 0;
+    let minDeadLineDay = 0;
     if (element.name === 'whichSite') {
         for (const item of formCalculate.elements) {
             if (item.type === 'checkbox') {
@@ -49,6 +75,9 @@ function priceCalculation(element) {
     for (const item of formCalculate.elements) {
         if (item.name === 'whichSite' && item.checked) {
             index = DATA.whichSite.indexOf(item.value);
+            maxDeadLineDay = DATA.deadLine[index][1];
+            minDeadLineDay = DATA.deadLine[index][0];
+            site = item.dataset.site;
         } else if (item.classList.contains('calc-handler') && item.checked) {
             options.push(item.value);
         }
@@ -69,15 +98,26 @@ function priceCalculation(element) {
         }
     });
     result += DATA.price[index];
-    totalPriceSum.textContent = result;
+    renderTextContent(result, site, maxDeadLineDay, minDeadLineDay);
 }
 
 function handlerCallBackForm(event) {
     const target = event.target;
+    for (let i = 0; i < inputArr.length; i++) {
+        if (inputArr[i].checked) {
+            checkboxLabel[i].textContent = "Да";
+        } else {
+            if (checkboxLabel[1].textContent == "Нет") {
+                checkboxLabel[2].textContent = "Нет";
+            }
+            checkboxLabel[i].textContent = "Нет";
+        }
+    }
     if (adapt.checked) {
         mobileTemplates.disabled = false;
     } else {
         mobileTemplates.disabled = true;
+        mobileTemplates.checked = false;
     }
     if (target.classList.contains('want-faster')) {
         (target.checked) ? showElem(fastRange) : hideElem(fastRange);
